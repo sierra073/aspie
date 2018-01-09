aef3e895896a72f12c2502ca0749c88da0d29076
Sierra Costanza <sierra@Sierras-MacBook-Pro.local>
2018-01-09 10:59:24 -0800

    WIP

--- get_reddit_data.py
+++ get_reddit_data.py
@@ -1,11 +1,18 @@
 from initialize_data import *
 from access_tokens import *
 import praw
+import psraw
 
 reddit = praw.Reddit(client_id=reddit_clientid,
                      client_secret=reddit_secret, password=reddit_password,
                      user_agent='aspie1', username=reddit_username)
 
-subreddit = reddit.subreddit('Ethereum').new(limit=10)
-for submission in subreddit:
-    print(submission.title)
+# subreddit = reddit.subreddit('Ethereum').new()
+
+# count = 0
+# for submission in subreddit:
+#     count+=1
+
+# print(count)
+
+print(len(list(psraw.submission_search(reddit, subreddit='Ethereum', limit = 250))))
