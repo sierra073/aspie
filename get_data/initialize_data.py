b0af882cbb75d1aaa117c2e54d927c6229908901
CryptoDash Server <>
2018-02-13 02:07:37 +0000

    WIP

--- get_data/initialize_data.py
+++ get_data/initialize_data.py
@@ -73,7 +73,7 @@ def insert_db(df,tablename):
     conn.close()
 
 #### Import input data with Protocols info 
-protocols = pd.read_csv("../data/protocols.csv")
+protocols = pd.read_csv("../data/input/protocols.csv")
 
 protocols['github_repos'] = protocols['github_repos'].apply(lambda x: stringToList(x))
 protocols['subreddits'] = protocols['subreddits'].apply(lambda x: stringToList(x))
