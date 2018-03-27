7ab232b895478fa8168a6a0e71aa18dede3bad13
CryptoDash Server <>
2018-03-27 02:45:02 +0000

    WIP

---
+++ get_data/get_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "$(date)"
+cd "/root/aspie/get_data/"
+python get_github_data.py &
+python get_github_stars.py &
+python get_reddit_data.py &
+python get_stackoverflow_data.py &
+python get_twitter_data.py &
+python get_searchinterest_data.py &
+python get_marketcap_history.py &
+python eth_address_count.py &
+python get_hackernews_data.py &
+python get_sentiments.py &
+wait
+python kpi.py &&
+python get_cbi_data.py &&
+echo "Done!"
