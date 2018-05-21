41f456cf9bbe8f06b5d89b794532be5cef20c4f5
Sierra Costanza <costanza.sierra@gmail.com>
2018-05-21 15:42:25 -0700

    debug twitter followers

--- get_data/get_twitter_data.py
+++ get_data/get_twitter_data.py
@@ -5,11 +5,12 @@ import twitter
 from bs4 import BeautifulSoup
 
 twitterapi = twitter.Api(consumer_key=twitter_consumer_key,
-              consumer_secret=twitter_consumer_secret,
-              access_token_key=twitter_access_token_key,
-              access_token_secret=twitter_access_token_secret)
+                         consumer_secret=twitter_consumer_secret,
+                         access_token_key=twitter_access_token_key,
+                         access_token_secret=twitter_access_token_secret)
 
-def get_twitter_status_data(x,max_dt):
+
+def get_twitter_status_data(x, max_dt):
 
     count = 100
     results = twitterapi.GetSearch(
@@ -17,41 +18,44 @@ def get_twitter_status_data(x,max_dt):
 
     data = pd.DataFrame([])
     for r in results:
-        tweet = pd.DataFrame({'date': r.created_at, 'id':r.id},index=[0])
+        tweet = pd.DataFrame({'date': r.created_at, 'id': r.id}, index=[0])
         tweet['date'] = pd.to_datetime(tweet['date'])
-        data = data.append(tweet,ignore_index=True)
+        data = data.append(tweet, ignore_index=True)
 
-    while (pd.to_datetime(data['date'].min(),unit='s') >= pd.to_datetime(max_dt)):
+    while (pd.to_datetime(data['date'].min(), unit='s') >= pd.to_datetime(max_dt)):
         min_id = data['id'].min()
         print(count)
 
         results = twitterapi.GetSearch(
-        raw_query="q=" + x + "&%23" + x + "&result_type=recent&" + "max_id=" + str(min_id) + "&since=" + max_dt + "&count=100")
-        
+            raw_query="q=" + x + "&%23" + x + "&result_type=recent&" + "max_id=" + str(min_id) + "&since=" + max_dt + "&count=100")
+
         for r in results:
-            tweet = pd.DataFrame({'date': r.created_at, 'id':r.id},index=[0])
+            tweet = pd.DataFrame({'date': r.created_at, 'id': r.id}, index=[0])
             tweet['date'] = pd.to_datetime(tweet['date'])
-            data = data.append(tweet,ignore_index=True)
-        
-        count+=100 # increment counter
+            data = data.append(tweet, ignore_index=True)
+
+        count += 100  # increment counter
 
     if not data.empty:
-            data = data.groupby('date').id.nunique().reset_index()
+        data = data.groupby('date').id.nunique().reset_index()
     else:
         data['date'] = max_dt
         data['id'] = 0
 
     return(data)
 
-def get_twitter_followers(url,max_dt):
+
+def get_twitter_followers(url, max_dt):
     r = requests.get(url)
-    soup = BeautifulSoup(r.content,"lxml")
+    print url
+    soup = BeautifulSoup(r.content, "lxml")
     f = soup.find('li', class_="ProfileNav-item--followers")
     title = f.find('a')['title']
-    num_followers = int(title.split(' ')[0].replace(',',''))
-    followers_dt =  pd.DataFrame({'date': pd.to_datetime('now'), 'followers': num_followers},index=[0])
+    num_followers = int(title.split(' ')[0].replace(',', ''))
+    followers_dt = pd.DataFrame({'date': pd.to_datetime('now'), 'followers': num_followers}, index=[0])
     return(followers_dt)
 
+
 twitter_followers = get_table_from_db('select * from twitter_followers;')
-api_wrapper_append(twitter_followers,get_twitter_followers,'Twitter',"https://twitter.com/","",'date',['follower_count'],True,False,'twitter_followers')
+api_wrapper_append(twitter_followers, get_twitter_followers, 'Twitter', "https://twitter.com/", "", 'date', ['follower_count'], True, False, 'twitter_followers')
 print("done")
